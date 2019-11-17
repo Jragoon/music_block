d1ee0f012c47c3a236fb20510f92e5af78f82976
Jared Taylor <jared.xyz@mailbox.org>
2019-11-17 15:57:58 -0500

    Blocks have individual state

--- init.lua
+++ init.lua
@@ -1,23 +1,28 @@
 sounds = {"a", "b", "c", "d", "e", "f", "g"}
-current_sound = 1
 
 minetest.register_node("music_block:music_block", {
     description = "A note from the past",
     tiles = {"note.png"},
     groups = {oddly_breakable_by_hand = 2},
+    current_sound = 1,
     on_punch = function(pos, node, puncher)
-		minetest.sound_play(sounds[current_sound], {
+        local meta = minetest.get_meta(pos)
+        local value = meta:get_int("current_sound")
+		minetest.sound_play(sounds[value], {
             pos = puncher:get_pos(),
             max_hear_distance = 10000,
             gain = 5.0
         })
-        current_sound = current_sound + 1
-        if current_sound > 7 then
-            current_sound = 1
+        value = value + 1
+        if value > 7 then
+            value = 1
         end
+        meta:set_int("current_sound", value)
     end,
     on_rightclick = function(pos, node, clicker)
-        minetest.sound_play(sounds[current_sound], {
+        local meta = minetest.get_meta(pos)
+        local value = meta:get_int("current_sound")
+        minetest.sound_play(sounds[value], {
             pos = clicker:get_pos(),
             max_hear_distance = 10000,
             gain = 5.0
